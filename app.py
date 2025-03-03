84e6c4ee33c4b8ce3a3f88245e99cfeac246f4f3
Reimanns <vanjakem@gmail.com>
2025-03-03 10:21:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -32,11 +32,11 @@ def format_date(dt):
 # ---------------------------
 status_data = {
     "All":       {"Canceled":10, "Released":37, "In Check":4, "Not Started":36, "Redlines":5, "In Progress":27},
-    "BDDS":      {"Canceled":0,  "Released":7,  "In Check":1, "Not Started":5,  "Redlines":5, "In Progress":13},
+    "BDDS":      {"Canceled":5,  "Released":8,  "In Check":1, "Not Started":5,  "Redlines":5, "In Progress":12},
     "Maverick":  {"Canceled":0,  "Released":23, "In Check":1, "Not Started":7,  "Redlines":0, "In Progress":0},
-    "Elisen":    {"Canceled":0,  "Released":4,  "In Check":0, "Not Started":3,  "Redlines":0, "In Progress":9},
-    "Cotney":    {"Canceled":3,  "Released":1,  "In Check":1, "Not Started":20, "Redlines":0, "In Progress":2},
-    "Citadel":   {"Canceled":2,  "Released":4,  "In Check":2, "Not Started":3,  "Redlines":0, "In Progress":2}
+    "Elisen":    {"Canceled":0,  "Released":6,  "In Check":1, "Not Started":5,  "Redlines":0, "In Progress":7},
+    "Cotney":    {"Canceled":3,  "Released":1,  "In Check":1, "Not Started":16, "Redlines":0, "In Progress":2},
+    "Citadel":   {"Canceled":2,  "Released":5,  "In Check":4, "Not Started":1,  "Redlines":0, "In Progress":1}
 }
 
 status_labels = ["Canceled", "Released", "In Check", "Not Started", "Redlines", "In Progress"]
